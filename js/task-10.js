15db798778e80d054cd9cfc59ab5e80989c8d89e
Vadym_Hermaniuk <54590021+wadja210894@users.noreply.github.com>
2023-02-25 16:26:01 +0200

    Update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyeBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('div#boxes');
+
+
+
+createBtn.addEventListener("click", onBtnCreateClickHandler);
+destroyeBtn.addEventListener("click", onBtnDestroyClickHandler);
+
+
+function onBtnCreateClickHandler() {
+  createBoxes(inputEl.value);
+}
+
+function onBtnDestroyClickHandler() {
+  boxes.innerHTML = '';
+}
+
+function createBoxes(amount) {
+  let arrayDiv = [];
+
+  for (let i = 1; i <= amount; i += 1) {
+    const div = document.createElement('div');
+    div.style.width = `${20 + (i * 10)}px`;
+    div.style.height = `${20 + (i * 10)}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    div.textContent = i;
+
+    arrayDiv.push(div);
+  };
+  boxes.append(...arrayDiv);
+};
